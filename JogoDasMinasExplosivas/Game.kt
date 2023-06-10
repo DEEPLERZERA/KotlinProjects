d07a05fa56808b512fd641f9a532041480508383
Daniel <dani2014games@gmail.com>
2023-06-09 23:45:58 -0300

    Making some changes

--- JogoDasMinasExplosivas/Game.kt
+++ JogoDasMinasExplosivas/Game.kt
@@ -1,30 +1,22 @@
-package minesweepernumMinemi
-
+package minesweeper
 import kotlin.random.Random
 
 fun main() {
-    val campo = Array(9) { Array(9) { '.' } }
-
     print("Quantas minas você quer que tenha no campo?: ")
-    val numMinas = readLine()?.toIntOrNull()
-
-    if (numMinas != null && numMinas in 0..81) {
-        var minaCount = 0
-
-        while (minaCount < numMinas) {
-            val fila = Random.nextInt(9)
-            val coluna = Random.nextInt(9)
-
-            if (campo[fila][coluna] != 'X') {
-                campo[fila][coluna] = 'X'
-                minaCount++
-            }
+    var numMinas = readLine()!!.toInt()
+    var quantMinas = 0
+    
+    for(i in 1..9)  {
+        var fila = Random.nextInt(1)
+        for(j in 1..9)  {
+         var coluna = Random.nextInt(1)
+            if(fila==coluna && quantMinas < numMinas) {
+                print("X")
+                quantMinas++
+            } else {
+                print(".")
+            }    
         }
-
-        for (fila in campo) {
-            println(fila.joinToString(""))
-        }
-    } else {
-        println("Número invalido de minas")
+        println()
     }
 }
