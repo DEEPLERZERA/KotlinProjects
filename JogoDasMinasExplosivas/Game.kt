91b3a9bae256bdb67d4516570dd6822748f13999
Daniel <dani2014games@gmail.com>
2023-06-23 01:20:47 -0300

    Making some changes

--- JogoDasMinasExplosivas/Game.kt
+++ JogoDasMinasExplosivas/Game.kt
@@ -1,22 +1,57 @@
 package minesweeper
+
 import kotlin.random.Random
 
 fun main() {
     print("Quantas minas você quer que tenha no campo?: ")
-    var numMinas = readLine()!!.toInt()
+    val numMinas = readLine()!!.toInt()
     var quantMinas = 0
     
-    for(i in 1..9)  {
-        var fila = Random.nextInt(1)
-        for(j in 1..9)  {
-         var coluna = Random.nextInt(1)
-            if(fila==coluna && quantMinas < numMinas) {
-                print("X")
-                quantMinas++
-            } else {
-                print(".")
-            }    
+    val campo = Array(9) { CharArray(9) { '.' } } // Inicializa o campo vazio
+    
+    // Coloca as minas aleatoriamente no campo
+    while (quantMinas < numMinas) {
+        val fila = Random.nextInt(9)
+        val coluna = Random.nextInt(9)
+        if (campo[fila][coluna] != 'X') {
+            campo[fila][coluna] = 'X'
+            quantMinas++
         }
-        println()
     }
+    
+    // Preenche o campo com o número de minas ao redor das células vazias
+    for (i in 0 until campo.size) {
+        for (j in 0 until campo[i].size) {
+            if (campo[i][j] == '.') {
+                val numMinasAoRedor = contarMinasAoRedor(campo, i, j)
+                if (numMinasAoRedor > 0) {
+                    campo[i][j] = numMinasAoRedor.toString()[0]
+                }
+            }
+        }
+    }
+    
+    // Imprime o campo
+    for (linha in campo) {
+        println(linha.joinToString(""))
+    }
+}
+
+// Função auxiliar para contar o número de minas ao redor de uma célula
+fun contarMinasAoRedor(campo: Array<CharArray>, fila: Int, coluna: Int): Int {
+    var count = 0
+    val minFila = maxOf(0, fila - 1)
+    val maxFila = minOf(fila + 1, campo.size - 1)
+    val minColuna = maxOf(0, coluna - 1)
+    val maxColuna = minOf(coluna + 1, campo[fila].size - 1)
+    
+    for (i in minFila..maxFila) {
+        for (j in minColuna..maxColuna) {
+            if (campo[i][j] == 'X') {
+                count++
+            }
+        }
+    }
+    
+    return count
 }
